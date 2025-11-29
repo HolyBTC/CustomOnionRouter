ecf3b857c088a01af742a415217698516dad83ca
SandrikaMgeli <kavlashvilisandro1@gmail.com>
2025-11-29 18:05:42 +0400

    Added api project for router

---
+++ OnionRouter.Api/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnionRouter.Api.Controllers;
+
+[Route("api/health")]
+[ApiController]
+public class HealthController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Get()
+    {
+        return NoContent();
+    }
+}
