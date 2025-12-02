b6b0b6df041483a7a5a6dde6badcc5b1df44a525
SandrikaMgeli <kavlashvilisandro1@gmail.com>
2025-12-03 01:05:04 +0400

    Add grpc ping request

---
+++ OnionRouter.Server/OnionRouter.Server.Api/GrpcServices/PingService.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+using OnionRouter.Contracts.Protos;
+
+namespace OnionRouter.Server.Api.GrpcServices;
+
+public class PingService : Contracts.Protos.PingService.PingServiceBase
+{
+    public override Task<PingReply> Ping(PingRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new PingReply());
+    }
+}
--- OnionRouter.Server/OnionRouter.Server.Api/Program.cs
+++ OnionRouter.Server/OnionRouter.Server.Api/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using PingService = OnionRouter.Server.Api.GrpcServices.PingService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddGrpc();
 builder.Services.AddOpenApi("docs");
-
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ConfigureEndpointDefaults(lo => lo.Protocols = HttpProtocols.Http2);
+});
 var app = builder.Build();
 
 app.MapOpenApi();
@@ -11,6 +18,7 @@ app.UseSwaggerUI(options =>
     options.SwaggerEndpoint("/openapi/docs.json", "OnionRouter.Server.Api");
 });
 
+app.MapGrpcService<PingService>();
 app.MapControllers();
 
 app.Run();
