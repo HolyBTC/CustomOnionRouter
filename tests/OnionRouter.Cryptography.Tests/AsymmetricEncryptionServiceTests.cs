8f0d44c40f1cc80011c2267434cafcc74ef8c172
Sandro Kavlashvili <sandro.kavlashvili@space.ge>
2025-12-03 14:42:04 +0400

    add unit tests for decryption

--- tests/OnionRouter.Cryptography.Tests/AsymmetricEncryptionServiceTests.cs
+++ tests/OnionRouter.Cryptography.Tests/AsymmetricEncryptionServiceTests.cs
@@ -55,6 +55,7 @@ public class AsymmetricEncryptionServiceTests
     [InlineData(5)]
     [InlineData(10)]
     [InlineData(15)]
+    [InlineData(20)]
     public void Encrypt_WhenSameKeyAndSameSizedText_ReturnsSameSizedEncryptedData(int length)
     {
         // Arrange
@@ -78,27 +79,6 @@ public class AsymmetricEncryptionServiceTests
             decryptedData.Should().BeEquivalentTo(plainData);
         }
     }
-
-    // [Theory]
-    // [InlineData("1234567891234567", "just random text 1", "Si7Fh9z405Jl85BybwZBVWw0N4peYyUf+jSIIlhk44I=")] // 128-bit key
-    // [InlineData("12345678912345671234567891234567", "just random text 1", "5oteI5e5EeYYh5MJsrNOBlYzTuFDOcTcPGSCxwdO/u4=")] // 256-bit key
-    // [InlineData("1234577891234561", "123 random", "3hbZscacLKYk1HKB/79Usg==")] // 128-bit key
-    // [InlineData("12345778912345611234577891234561", "123 random", "NOcslQKwPaAEUkVA8Qn1Sg==")] // 256-bit key
-    // [InlineData("1234562891234567", "just random text 1", "PBpk+zk168evNGSXj+dPMx/fax5Is0orikgcx6n24XE=")] // 128-bit key
-    // [InlineData("12345628912345671234562891234567", "just random text 1", "Sth2eXpM6eOEB0zHCj/hsHRUUduK286hqR2HBDMdlJI=")] // 256-bit key
-    // [InlineData("1234111891234567", "just random text 1", "XrjKFopZqXAKsF2cwJ53yskLFn4JIUhGT4m5prY4JnY=")] // 128-bit key
-    // [InlineData("12341118912345671234111891234567", "just random text 1", "r2n1/DvdOSY+nyMCVk4BlKqXaMDiWPd4p3iEr3Mjc68=")] // 256-bit key
-    // public void Encrypt_EncryptsCorrectly(string publicKey, string privateKey, string plainText, string expectedEncryptedDataAsBase64)
-    // {
-    //     // Act
-    //     byte[] returnedBytes = _asymmetricEncryptionService.Encrypt(Encoding.UTF8.GetBytes(plainText), Convert.FromBase64String(publicKey));
-    //     byte[] decryptedBytes = _asymmetricEncryptionService.Decrypt(returnedBytes, Convert.FromBase64String(privateKey));
-    //
-    //     // Assert
-    //     returnedBytes.Should().NotBeNull();
-    //     returnedBytes.Should().BeEquivalentTo(Convert.FromBase64String(expectedEncryptedDataAsBase64));
-    //     decryptedBytes.Should().BeEquivalentTo(Encoding.UTF8.GetBytes(plainText));
-    // }
 
     [Theory]
     [InlineData(true)]
