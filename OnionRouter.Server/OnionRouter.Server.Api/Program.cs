1272ae4a748ca024c7849bc20cee8f37139157ed
Sandro Kavlashvili <sandro.kavlashvili@space.ge>
2025-12-10 11:45:14 +0400

    remove grpc support

--- OnionRouter.Server/OnionRouter.Server.Api/Program.cs
+++ OnionRouter.Server/OnionRouter.Server.Api/Program.cs
@@ -1,15 +1,8 @@
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using PingService = OnionRouter.Server.Api.GrpcServices.PingService;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddGrpc();
 builder.Services.AddOpenApi("docs");
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ConfigureEndpointDefaults(lo => lo.Protocols = HttpProtocols.Http2);
-});
+
 var app = builder.Build();
 
 app.MapOpenApi();
@@ -18,7 +11,6 @@ app.UseSwaggerUI(options =>
     options.SwaggerEndpoint("/openapi/docs.json", "OnionRouter.Server.Api");
 });
 
-app.MapGrpcService<PingService>();
 app.MapControllers();
 
 app.Run();
